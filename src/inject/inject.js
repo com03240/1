7a65df080f6f0fa46e35f5f2f03526e43909b322
dantony <dantonio87@gmail.com>
2015-07-28 20:39:54 -0400

    Popup command template...
    Popup UI changes, also still need to commit jQuery and jQuery-ui...

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,44 +1,16 @@
 // check ready state
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-		if (document.readyState === "complete") {
-			clearInterval(readyStateCheckInterval);
-		}
-	}, 10);
+	$(document).ready(function(){
+		console.log("ready!");
+	})
 });
 
 // this is the controller
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 	var video = document.getElementsByTagName("video")[0];
-	if (message.action === "exec_exscript") {
-		// parse and execute the script
-		var exscript = parse_exscript(message.script);
-		if (exscript.length > 0) {
-			// clear currently executing script threads
-			exec_clear();
-			// press play
-			video.play();
-			// execute the script
-			exec_exscript(exscript, video);
-		}
-	} else if (message.action === "exec_clear") {
-		// clear currently executing script thread
-		exec_clear();
-		// reset rate
-		video.playbackRate = 1;
-	} else if (message.action === "is_paused") {
-		// check for paused
-		sendResponse({ is_paused: video.paused });
-	} else if (message.action == "toggle_play") {
-		// press play
-		video.play();
+	if (message.action == "get_video_data") {
 		// inform the page action of success
-		sendResponse({ is_paused: video.paused });
-	} else if (message.action == "toggle_pause") {
-		// press pause
-		video.pause();
-		// inform the page action of success
-		sendResponse({ is_paused: video.paused });
+		sendResponse({ video_data: video.duration });
 	}
 });
 
--- src/page_action/page_action.js
+++ src/page_action/page_action.js
@@ -1,203 +1,80 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	// http://stackoverflow.com/a/3560038
-	var imgURL = chrome.extension.getURL("icons/icon128.png");
-	document.getElementById("logo").src = imgURL;
+$(document).ready(function(){
+	console.log("ready!");
+	/*
+	$.widget( "ui.timespinner", $.ui.spinner, {
+		options: {
+			// seconds
+			step: 60 * 1000,
+			// hours
+			page: 60
+		},
+		_parse: function( value ) {
+			console.log(hhmmss_to_ss(value));
+			return value;
+		},
+		_format: function( value ) {
+			console.log(value);
+			return value;
+		}
+	});
+	*/
 	
-	// UI handles
-	var select = document.getElementById("ex_select");
-	var script_name = document.getElementById("ex_label");
-	var script_text = document.getElementById("ex_editor");
+	console.log(hhmmss_to_ss("00:00:00"));
+	console.log(hhmmss_to_ss("00:00:01"));
+	console.log(hhmmss_to_ss("00:01:00"));
+	console.log(hhmmss_to_ss("01:00:00"));
 	
-	// current tab object
-	var current_tab = null;
-	
-	// unicode symbols
-	var play_symbol = "\u25B6";
-	var pause_symbol = "\u25AE\u25AE";
-	
-	// initialize the video toggle button
+	// initialize popup data
+	var video_data = null;
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(
 			tabs[0].id, { 
-				action: "is_paused"
+				action: "get_video_data"
 			}, function(response) {
-				console.log(response);
-				var innerHTML = (response.is_paused) ? play_symbol : pause_symbol;
-				document.getElementById("ex_play").innerHTML = innerHTML;
+				video_data = response.video_data;
 			}
 		);
 	});
 	
-	/**
-	 * Get the URL for this video to store corresponding scripts by ID.
-	 *
-	 * @param url {string} the URL
-	 * @return {string} the v parameter
-	 */
-	function get_video_id(url) {
-		var v = null;
-		url.slice(url.indexOf("?") + 1).split("&").forEach(function(param) {
-			var tokens = param.split("=");
-			v = (tokens[0] == "v") ? tokens[1] : v;
-		});
-		return v;
-	}
-	
-	/**
-	 * Get popup data from UI controls.
-	 *
-	 * @return {object} the popup data object
-	 */
-	function get_popup_data() {
-		var idx = select.selectedIndex;
-		return {
-			"selected": (idx > -1) ? select.options[idx].value : null,
-			"script_name": script_name.value,
-			"script_text": script_text.value
-		};
-	}
-	
-	/**
-	 * Add an option to the select UI.
-	 *
-	 * @param {string} the option text
-	 */
-	function add_option(text) {
-		var option = document.createElement("option");
-		option.text = text;
-		select.add(option);
-	}
-	
-	// initialize the popup
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		// query chrome storage for the script storage object
-		console.log("init >");
-		current_tab = tabs[0];
-		var key = get_video_id(current_tab.url);
-		var query = {};
-		query[key] = {};
-		query[key]["scripts"] = {};
-		console.log("query: " + JSON.stringify(query));
-		// initialize the UI controls based on the scripts obtained from storage
-		chrome.storage.sync.get(query, function(obj) {
-			// select UI
-			for (var name in obj[key]["scripts"]) {
-				add_option(name);
-			}
-			// input and editor UI
-			var data = get_popup_data();
-			if (data.selected !== null) {
-				script_name.value = data.selected;
-				script_text.value = obj[key]["scripts"][data.selected];
+	$("#addcmd").click(function(event) {
+		// clone command template and make visible
+		var cmd = $("#templatecmd").clone().css("display", true).attr("id", "x");
+		// initialize time spinner
+		console.log(cmd.children('input[class^="time"]'));
+		cmd.children('input[class^="time"]').timespinner();
+		// initialize slider
+		cmd.children(".slider-range").slider({
+			range: true,
+			min: 0,
+			max: 500,
+			values: [ 75, 300 ],
+			slide: function( event, ui ) {
+				console.log(ui);
 			}
 		});
+		cmd.insertBefore("#addcmd");
 	});
-	
-	// initialize editor on selection of a script from the select UI
-	select.onchange = function(event) {
-		console.log("onchange >");
-		var key = get_video_id(current_tab.url);
-		var query = {};
-		query[key] = {};
-		query[key]["scripts"] = {};
-		console.log(query);
-		chrome.storage.sync.get(query, function(obj) {
-			console.log(obj);
-			script_name.value = event.target.value;
-			script_text.value = obj[key]["scripts"][event.target.value];
+})
+
+/**
+ * Convert the time token into seconds.
+ * The token may specify seconds up to the number of hours.
+ * 
+ * @param {string} the time
+ * @return {number} the number of seconds
+ */
+function hhmmss_to_ss(hhmmss) {
+	var tokens = hhmmss.split(":");
+	var index_to_multiplier = [60 * 60, 60, 1];
+	sum = 0;
+	if (tokens.length <= 3) {
+		$.each(tokens, function(index, value) {
+			sum += index_to_multiplier[index] * parseInt(value);
 		});
-	};
-	
-	// stop execution of the current script
-	document.getElementById("ex_stop").onclick = function(event) {
-		var message = {
-			action: "exec_clear"
-		};
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, message, function(response) {});
-		});
-	};
-	
-	
-	// run the current script
-	document.getElementById("ex_run").onclick = function(event) {
-		var message = {
-			action: "exec_exscript",
-			script: script_text.value
-		};
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, message, function(response) {});
-		});
-	};
-	
-	// save the current script
-	document.getElementById("ex_save").onclick = function(event) {
-		// query storage to get and alter the object
-		console.log("save >");
-		var key = get_video_id(current_tab.url);
-		var query = {};
-		query[key] = {};
-		query[key]["scripts"] = {};
-		console.log("query: " + JSON.stringify(query));
-		// get
-		chrome.storage.sync.get(query, function(obj) {
-			var data = get_popup_data();
-			// alter
-			obj[key]["scripts"][data.script_name] = data.script_text;
-			chrome.storage.sync.set(obj, function() {
-				// update UI
-				if (data.script_name !== data.selected) {
-					add_option(data.script_name);
-					select.value = data.script_name;
-				}
-			});
-		});
-	};
-	
-	// delete the current script from storage
-	document.getElementById("ex_delete").onclick = function(event) {
-		// query object
-		var key = get_video_id(current_tab.url);
-		var query = {};
-		query[key] = {};
-		query[key]["scripts"] = {};
-		chrome.storage.sync.get(query, function(obj) {
-			var data = get_popup_data();
-			// alter
-			delete obj[key]["scripts"][data.script_name];
-			chrome.storage.sync.set(obj, function() {
-				// update UI
-				select.remove(select.selectedIndex);
-				if (select.options.length > 0) {
-					var event = {
-						"target": { "value": select.options[0].value }
-					};
-					select.onchange(event);
-				} else {
-					script_name.value = "";
-					script_text.value = "";
-				}
-			});
-		});
-	};
-	
-	// send message to play/pause the video
-	document.getElementById("ex_play").onclick = function(event) {
-		var message = {};
-		if (event.target.innerHTML === play_symbol) {
-			event.target.innerHTML = pause_symbol;
-			message["action"] = "toggle_play";
-		} else {
-			event.target.innerHTML = play_symbol;
-			message["action"] = "toggle_pause";
-		}
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
-				var innerHTML = (response.is_paused) ? play_symbol : pause_symbol;
-				event.target.innerHTML = innerHTML;
-			});
-		});
-	};
-});
+	} else {
+		sum = -1;
+	}
+	return sum;
+}
 
 
