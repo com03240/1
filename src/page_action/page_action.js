67e226c008eeef03ae405e89347b8073d51622e1
dantony <dantonio87@gmail.com>
2015-08-03 21:57:15 -0400

    Popup UI logic and testing...
    The popup is now a standalone test page...

--- src/page_action/page_action.js
+++ src/page_action/page_action.js
@@ -1,80 +1,126 @@
 $(document).ready(function(){
 	console.log("ready!");
-	/*
-	$.widget( "ui.timespinner", $.ui.spinner, {
-		options: {
-			// seconds
-			step: 60 * 1000,
-			// hours
-			page: 60
-		},
-		_parse: function( value ) {
-			console.log(hhmmss_to_ss(value));
-			return value;
-		},
-		_format: function( value ) {
-			console.log(value);
-			return value;
-		}
-	});
-	*/
-	
-	console.log(hhmmss_to_ss("00:00:00"));
-	console.log(hhmmss_to_ss("00:00:01"));
-	console.log(hhmmss_to_ss("00:01:00"));
-	console.log(hhmmss_to_ss("01:00:00"));
 	
 	// initialize popup data
-	var video_data = null;
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(
-			tabs[0].id, { 
-				action: "get_video_data"
-			}, function(response) {
-				video_data = response.video_data;
-			}
-		);
-	});
+	var video_data = 60 * 3;
+	
+	/*
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.sendMessage(
+				tabs[0].id, { 
+					action: "get_video_data"
+				}, function(response) {
+					video_data = response.video_data;
+				}
+			);
+		});
+	*/
 	
 	$("#addcmd").click(function(event) {
 		// clone command template and make visible
-		var cmd = $("#templatecmd").clone().css("display", true).attr("id", "x");
+		var id_suffix = $(".command").size();
+		var cmd = $("#templatecmd").clone();
+		cmd.addClass("command");
+		cmd.css("display", true);
+		cmd.attr("id", cmd.attr("class") + "_" + id_suffix);
 		// initialize time spinner
-		console.log(cmd.children('input[class^="time"]'));
-		cmd.children('input[class^="time"]').timespinner();
-		// initialize slider
+		$.each(cmd.children(), function(index, value) {
+			$(value).attr("id", $(value).attr("class") + "_" + id_suffix);
+		});
+		cmd.children(".time1").timespinner({
+			min: ss_to_hhmmss(0),
+			max: ss_to_hhmmss(video_data),
+			change: on_change,
+			spin: on_spin
+		}).val(ss_to_hhmmss(video_data * 0.25));
+		cmd.children(".time2").timespinner({
+			min: ss_to_hhmmss(0),
+			max: ss_to_hhmmss(video_data),
+			change: on_change,
+			spin: on_spin
+		}).val(ss_to_hhmmss(video_data * 0.75));
+		// initialize slider range
 		cmd.children(".slider-range").slider({
 			range: true,
 			min: 0,
-			max: 500,
-			values: [ 75, 300 ],
-			slide: function( event, ui ) {
-				console.log(ui);
-			}
+			max: video_data,
+			values: [video_data * 0.25, video_data * 0.75],
+			slide: on_slide
 		});
-		cmd.insertBefore("#addcmd");
+		// initialize loop spinner
+		cmd.children(".loop").loopspinner();
+		// initialize rate spinner
+		cmd.children(".rate").ratespinner();
+		cmd.appendTo("#commands");
 	});
 })
 
-/**
- * Convert the time token into seconds.
- * The token may specify seconds up to the number of hours.
- * 
- * @param {string} the time
- * @return {number} the number of seconds
- */
-function hhmmss_to_ss(hhmmss) {
-	var tokens = hhmmss.split(":");
-	var index_to_multiplier = [60 * 60, 60, 1];
-	sum = 0;
-	if (tokens.length <= 3) {
-		$.each(tokens, function(index, value) {
-			sum += index_to_multiplier[index] * parseInt(value);
-		});
-	} else {
-		sum = -1;
+function on_change(event, ui) {
+	var tokens = event.target.id.split("_");
+	
+	var t0 = timespinner_parse(event.target.value);
+	var t1 = $("#time1_" + tokens[1]).timespinner("value");
+	var t2 = $("#time2_" + tokens[1]).timespinner("value");
+	var tm = $("#slider-range_" + tokens[1]).slider("option", "max");
+	
+	if (tokens[0] == "time1") {
+		if (0 <= t0 && t1 <= t2) {
+			$("#slider-range_" + tokens[1]).slider("values", 0, t0);
+			$("#time1_" + tokens[1]).timespinner("value", t0);
+		} else {
+			$("#time1_" + tokens[1]).timespinner(
+				"value", 
+				$("#slider-range_" + tokens[1]).slider("values", 0)
+			);
+		}
 	}
-	return sum;
+	if (tokens[0] == "time2") {
+		if (t1 <= t0 && t0 <= tm) {
+			$("#slider-range_" + tokens[1]).slider("values", 1, t0);
+			$("#time2_" + tokens[1]).timespinner("value", t0);
+		} else {
+			$("#time2_" + tokens[1]).timespinner(
+				"value", 
+				$("#slider-range_" + tokens[1]).slider("values", 1)
+			);
+		}
+	}
+}
+
+function on_slide(event, ui) {
+	var tokens = event.target.id.split("_");
+	$("#time1_" + tokens[1]).timespinner("value", ui.values[0]);
+	$("#time2_" + tokens[1]).timespinner("value", ui.values[1]);
+}
+
+// http://stackoverflow.com/a/21025562
+// http://stackoverflow.com/a/13250459
+function on_spin(event, ui) {
+	var tokens = $(this).attr("id").split("_");
+	var t0 = ui.value;
+	var t1 = $("#time1_" + tokens[1]).timespinner("value");
+	var t2 = $("#time2_" + tokens[1]).timespinner("value");
+	var value = -1;
+	var handle = -1;
+	if (tokens[0] == "time1") {
+		if (t0 <= t2) {
+			value = t0;
+			handle = 0;
+		} else {
+			event.preventDefault();
+		}
+	}
+	if (tokens[0] == "time2") {
+		if (t1 <= t0) {
+			value = t0;
+			handle = 1;
+		} else {
+			event.preventDefault();
+		}
+	}
+	if (value > -1) {
+		$("#slider-range_" + tokens[1]).slider("values", handle, value);
+	}
 }
 
 
---
+++ src/page_action/util.js
@@ -0,0 +1,115 @@
+/**
+ * Convert the time token into seconds.
+ * The token may specify seconds up to the number of hours.
+ * 
+ * @param {string} the time
+ * @return {number} the number of seconds
+ */
+function hhmmss_to_ss(hhmmss) {
+	// split
+	var tokens = hhmmss.split(":").map(function(e) {
+		// convert empty strings to 0
+		return e === "" ? 0 : e;
+	});
+	var ss = 0;
+	if (tokens.length <= 3) {
+		var index_to_multiplier = [60 * 60, 60, 1].slice(3 - tokens.length);
+		$.each(tokens, function(index, value) {
+			ss += index_to_multiplier[index] * parseInt(value);
+		});
+	} else {
+		ss = -1;
+	}
+	return ss;
+}
+
+/**
+ * Convert seconds into a time string.
+ * 
+ * @return {number} the number of seconds
+ * @param {string} the time
+ */
+function ss_to_hhmmss(ss) {
+	var hh = 0;
+	if (ss >= 3600) {
+		hh = Math.floor(ss / 3600);
+		ss -= hh * 3600;
+	}
+	var mm = 0;
+	if (ss >= 60) {
+		mm = Math.floor(ss / 60);
+		ss -= mm * 60
+	}
+	var hh_pad = (hh < 10) ? "0" : "";
+	var mm_pad = (mm < 10) ? "0" : "";
+	var ss_pad = (ss < 10) ? "0" : "";
+	return hh_pad + hh + ":" + mm_pad + mm + ":" + ss_pad + ss;
+}
+
+function timespinner_parse(value) {
+	// console.log("parse > " + value);
+	if (typeof value === "string") {
+		// already a timestamp
+		if (Number(value) == value) {
+			return Number(value);
+		}
+		return hhmmss_to_ss(value);
+	}
+	return value;
+}
+
+$.widget("ui.timespinner", $.ui.spinner, {
+	options: {
+		// seconds
+		step: 1,
+		// minutes
+		page: 60
+	},
+	_parse: timespinner_parse,
+	_format: function(value) {
+		return ss_to_hhmmss(value);
+	}
+});
+
+// http://stackoverflow.com/a/17708266
+$.widget("ui.loopspinner", $.ui.spinner, {
+	_format: function(value) {
+		return value + "x";
+	},
+	_parse: function(value) {
+		return parseInt(value);
+	}
+});
+
+$.widget("ui.ratespinner", $.ui.spinner, {
+	options: {
+		// seconds
+		step: 1,
+		// minutes
+		page: 10
+	},
+	_format: function(value) {
+		return value + "%";
+	},
+	_parse: function(value) {
+		return parseFloat(value);
+	}
+});
+
+// test
+console.log(hhmmss_to_ss(":00"));
+console.log(hhmmss_to_ss(":01"));
+console.log(hhmmss_to_ss("00:00"));
+console.log(hhmmss_to_ss("00:01"));
+console.log(hhmmss_to_ss("01:00"));
+console.log(hhmmss_to_ss("00:00:00"));
+console.log(hhmmss_to_ss("00:00:01"));
+console.log(hhmmss_to_ss("00:01:00"));
+console.log(hhmmss_to_ss("01:00:00"));
+console.log(ss_to_hhmmss(0));
+console.log(ss_to_hhmmss(60));
+console.log(ss_to_hhmmss(3600));
+console.log(ss_to_hhmmss(3660));
+console.log(ss_to_hhmmss(3661));
+
+
